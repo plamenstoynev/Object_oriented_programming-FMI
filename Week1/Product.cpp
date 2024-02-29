53f2bd62256385bb712123f00afac9ae42a56dde
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-02-29 15:31:38 +0200

    Add Product.cpp

---
+++ Week1/Product.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct Product{
+    char description[32];
+    int partNum;
+    double cost;
+};
+
+void inputProduct(Product& product){
+    std::cin.ignore();
+    std::cin.getline(product.description, '/n');
+    std::cin >> product.partNum;
+    std::cin >> product.cost;
+}
+
+
+
+int main(){
+    size_t numOfProducts = 5;
+    Product products[numOfProducts];
+    for (size_t i = 0; i < numOfProducts; ++i) {
+        inputProduct(products[i]);
+    }
+
+    for (size_t i = 0; i < numOfProducts; ++i) {
+        std::cout << products[i].partNum << std::endl;
+    }
+    return 0;
+}
