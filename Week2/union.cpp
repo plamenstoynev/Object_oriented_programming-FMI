db8f41776efebc79b81f0278f6d2663d80384f98
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-03-05 17:20:34 +0200

    update union.cpp

--- Week2/union.cpp
+++ Week2/union.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 struct Point{
     int x;
@@ -10,11 +11,16 @@ void inputPoint(Point& point){
     std::cin >>point.y;
 }
 
+double distancePoints(const Point& point1,const Point& point2){
+    double dx = point2.x - point1.x;
+    double dy = point2.y - point1.y;
 
-struct Triangle{
+    return std::sqrt((dx * dx) + (dy * dy));
+}
+
+struct Square{
     Point point1;
     Point point2;
-    Point point3;
 };
 
 struct Circle{
@@ -28,16 +34,33 @@ struct Rectangle{
 };
 
 enum class ShapeType{
-    Triangle,
+    Square,
     Circle,
     Rectangle
 };
 
 union Shape{
-    Triangle triangle;
+    Square square;
     Circle circle;
     Rectangle rectangle;
 };
+
+double areaOfShape(const Shape& shape, const ShapeType& type){
+    double area;
+    double PI = 3.14;
+    switch (type) {
+        case ShapeType::Square:
+            area = distancePoints(shape.square.point1, shape.square.point2) * distancePoints(shape.square.point1, shape.square.point2);
+            break;
+        case ShapeType::Rectangle:
+            area = distancePoints(shape.rectangle.point1, shape.rectangle.point2) * distancePoints(shape.rectangle.point1, shape.rectangle.point2);
+            break;
+        case ShapeType::Circle:
+            area = PI * shape.circle.r * shape.circle.r;
+            break;
+    }
+    return area;
+}
 
 int main(){
 
