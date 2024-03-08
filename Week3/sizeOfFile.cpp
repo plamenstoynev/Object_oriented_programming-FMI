459787af0d6bb0e70f27992d38fe67f70403dfd2
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-03-08 10:58:24 +0200

    add Week3 and sizeOfFile.cpp

---
+++ Week3/sizeOfFile.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+unsigned sizeOfFile(const char* FILE_NAME){
+    std::ifstream file(FILE_NAME);
+    unsigned size;
+    unsigned temp;
+
+    if(!file.is_open())
+        return -1;
+
+    temp = file.tellg();
+
+    file.seekg(std::ios::end);
+    size = file.tellg();
+
+    file.seekg(temp);
+    file.clear();
+
+    return size;
+}
+
+int main(){
+    std::cout << sizeOfFile("sizeOfFile.cpp") << std::endl;
+}
