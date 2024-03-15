1d6436ef9f52cd7308c0c13398a8cbe357e4505e
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-03-15 15:46:20 +0200

    update storeBinary.cpp

--- Week4/storeBinary.cpp
+++ Week4/storeBinary.cpp
@@ -7,13 +7,46 @@ struct Product{
     char* name;
     unsigned quantity;
     double price;
+
+    Product() = default;
+    Product(unsigned barcode, const char* name, unsigned quantity, double price){
+        this->barcode = barcode;
+        this->name = new char[strlen(name)];
+        strcpy(this->name, name);
+        this->quantity = quantity;
+        this->price = price;
+    }
+
+    void free(){
+        barcode = 0;
+        delete[] name;
+        name = nullptr;
+        quantity = 0;
+        price = 0;
+    }
 };
 
 struct Store{
     char name[30];
     char address[50];
     Product* products;
+    double profit;
+
+    Store() = default;
+    Store(const char* name, const char* address, Product* products, double profit){
+        strcpy(this->name, name);
+        strcpy(this->address, address);
+        this->profit = profit;
+    }
+
+    void free(){
+        delete[] products;
+        products = nullptr;
+        profit = 0;
+    }
 };
+
+
 
 
 
