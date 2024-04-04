b3eb0a961b9c9f5a33e042e3b6a7cc87fb02ac5a
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-04-04 07:17:15 +0300

    add BelotTournament

---
+++ Week6/BelotTournament/SoftwareEngineer.h
@@ -0,0 +1,14 @@
+//
+// Created by plame on 4/4/2024.
+//
+
+#ifndef OOP_PRACT_SOFTWAREENGINEER_H
+#define OOP_PRACT_SOFTWAREENGINEER_H
+
+
+class SoftwareEngineer {
+
+};
+
+
+#endif //OOP_PRACT_SOFTWAREENGINEER_H
